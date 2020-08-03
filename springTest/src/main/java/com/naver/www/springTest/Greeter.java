11e584762f0ca0f97871eb2b61ee1128f713348a
soldesk <soldesk@DESKTOP-0NOF839>
2020-08-03 15:55:05 +0900

    test2

---
+++ springTest/src/main/java/com/naver/www/springTest/Greeter.java
@@ -0,0 +1,15 @@
+package com.naver.www.springTest;
+
+public class Greeter {
+	private String format; {
+		
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+}
